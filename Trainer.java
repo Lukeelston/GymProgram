23f60323d293e0ebb4ba795b84394a3a02c2de3d
Joshuamelton36 <156853527+Joshuamelton36@users.noreply.github.com>
2024-04-23 13:05:00 -0400

    added an overide thing

--- Trainer.java
+++ Trainer.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package trainerprogram;
+
 
 /**
  *
@@ -80,5 +80,9 @@ public class Trainer {
     public String Hall(){
         return hall;
     }
-    
+    @Override
+    public String toString(){
+        return String.format("Trainer Name: " + name + " | Train type: " + trainType + " | Trainer Hall: " + hall + "\n");
+    }
 }
+@Override
